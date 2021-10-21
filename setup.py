868f65f2ef5b4f022a6ed476c0c49e10af5730f0
Tobias Kaymak <tobias.kaymak@ricardo.ch>
2021-10-21 12:35:54 +0000

    sme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ray-gcs',
-    version='0.0.3',
+    version='0.0.4',
     author='Tobias Kaymak',
     author_email='tobias.kaymak@gmail.com',
     description='Ray KV GCS addon',
